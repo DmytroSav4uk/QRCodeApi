b7c38575ea2260d1999f3dc86b161ee5deb5480f
Dmytro Savchuk <dmytro.savchuk.ua@gmail.com>
2025-04-20 20:26:01 +0300

    allow cors

--- QRCodeApi/Program.cs
+++ QRCodeApi/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+
+
 builder.Services.AddControllers();
 var app = builder.Build();
 
+app.UseCors("AllowAll");
 app.MapControllers();
 app.Run();
