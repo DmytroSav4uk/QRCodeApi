4d6ffd14e7a8d8921243d7c95fb5d7fd3918e50a
Dmytro Savchuk <dmytro.savchuk.ua@gmail.com>
2025-05-13 19:15:40 +0300

    +builder

--- QRCodeApi/Controllers/QrController.cs
+++ QRCodeApi/Controllers/QrController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using QRCoder;
 using ZXing.QrCode.Internal;
 using ZXing.Windows.Compatibility;
+using QRCodeApi.Services;
 
 
 namespace QRCodeApi.Controllers
@@ -19,10 +20,10 @@ namespace QRCodeApi.Controllers
             public string BackgroundColor { get; set; } = "#FFFFFF";
             public string? LogoBase64 { get; set; }
             public bool Bubble { get; set; } = false; 
+            
+            public bool TransparentBackground { get; set; } = false;
         }
-
-
-
+        
         public class QrResponse
         {
             public string Base64Png { get; set; }
@@ -34,31 +35,21 @@ namespace QRCodeApi.Controllers
         }
         
         
-      
         [HttpPost("generateCode")]
         public IActionResult GenerateQrCode([FromBody] QrRequest request)
         {
             if (string.IsNullOrWhiteSpace(request.Url))
                 return BadRequest("URL is required.");
 
-            Bitmap qrBitmap = request.Bubble
-                ? GenerateBubbleQrCode(request)
-                : GenerateStandardQrCode(request);
+            var qrBitmap = QrCodeBuilder
+                .Create(request.Url)
+                .WithPixelSize(request.PixelSize)
+                .WithColors(request.ForegroundColor, request.BackgroundColor)
+                .WithBubbleStyle(request.Bubble)
+                .WithLogo(request.LogoBase64)
+                .WithTransparentBackground(request.TransparentBackground) 
+                .Build();
 
-            if (!string.IsNullOrWhiteSpace(request.LogoBase64))
-            {
-                byte[] logoBytes = Convert.FromBase64String(request.LogoBase64);
-                using var logoStream = new MemoryStream(logoBytes);
-                using var logo = new Bitmap(logoStream);
-
-                int logoSize = qrBitmap.Width / 5;
-                using var resizedLogo = new Bitmap(logo, new Size(logoSize, logoSize));
-
-                using Graphics g = Graphics.FromImage(qrBitmap);
-                int x = (qrBitmap.Width - resizedLogo.Width) / 2;
-                int y = (qrBitmap.Height - resizedLogo.Height) / 2;
-                g.DrawImage(resizedLogo, x, y, resizedLogo.Width, resizedLogo.Height);
-            }
 
             using var outputStream = new MemoryStream();
             qrBitmap.Save(outputStream, System.Drawing.Imaging.ImageFormat.Png);
@@ -66,83 +57,6 @@ namespace QRCodeApi.Controllers
 
             return Ok(new QrResponse { Base64Png = base64 });
         }
-
-        
-        
-        private Bitmap GenerateStandardQrCode(QrRequest request)
-        {
-            using var generator = new QRCodeGenerator();
-            using var qrData = generator.CreateQrCode(request.Url, QRCodeGenerator.ECCLevel.Q);
-
-            var qrCode = new PngByteQRCode(qrData);
-            byte[] pngBytes = qrCode.GetGraphic(request.PixelSize);
-
-            using var ms = new MemoryStream(pngBytes);
-            using var qrBitmap = new Bitmap(ms);
-
-            return RecolorBitmap(qrBitmap, request.ForegroundColor, request.BackgroundColor);
-        }
-        private Bitmap GenerateBubbleQrCode(QrRequest request)
-        {
-            using var generator = new QRCodeGenerator();
-            var qrData = generator.CreateQrCode(request.Url, QRCodeGenerator.ECCLevel.Q);
-            var modules = qrData.ModuleMatrix;
-
-            int moduleSize = request.PixelSize;
-            int qrSize = modules.Count;
-            int imgSize = qrSize * moduleSize;
-
-            var fgColor = ColorTranslator.FromHtml(request.ForegroundColor);
-            var bgColor = ColorTranslator.FromHtml(request.BackgroundColor);
-
-            var bitmap = new Bitmap(imgSize, imgSize);
-            using var g = Graphics.FromImage(bitmap);
-            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-            g.Clear(bgColor);
-
-            using var brush = new SolidBrush(fgColor);
-
-            for (int y = 0; y < qrSize; y++)
-            {
-                for (int x = 0; x < qrSize; x++)
-                {
-                    if (modules[y][x])
-                    {
-                        int px = x * moduleSize;
-                        int py = y * moduleSize;
-                        g.FillEllipse(brush, px, py, moduleSize, moduleSize);
-                    }
-                }
-            }
-
-            return bitmap;
-        }
-        
-        private static Bitmap RecolorBitmap(Bitmap original, string fgHex, string bgHex)
-        {
-            Color fgColor = ColorTranslator.FromHtml(fgHex);
-            Color bgColor = ColorTranslator.FromHtml(bgHex);
-
-            Bitmap recolored = new Bitmap(original.Width, original.Height);
-            using Graphics g = Graphics.FromImage(recolored);
-            g.Clear(bgColor);
-
-            for (int y = 0; y < original.Height; y++)
-            {
-                for (int x = 0; x < original.Width; x++)
-                {
-                    Color pixel = original.GetPixel(x, y);
-                    if (pixel.R == 0 && pixel.G == 0 && pixel.B == 0)
-                    {
-                        recolored.SetPixel(x, y, fgColor);
-                    }
-                }
-            }
-
-            return recolored;
-        }
-
-
         
         
         [HttpPost("readCode")]
@@ -162,7 +76,5 @@ namespace QRCodeApi.Controllers
 
             return Ok(new QrReadResponse { Text = result.Text });
         }
-
-        
     }
 }
---
+++ QRCodeApi/Services/QrCodeBuilder.cs
@@ -0,0 +1,150 @@
+﻿using QRCoder;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace QRCodeApi.Services
+{
+    public class QrCodeBuilder
+    {
+        private string _url;
+        private int _pixelSize = 20;
+        private string _foregroundColor = "#000000";
+        private string _backgroundColor = "#FFFFFF";
+        private string _logoBase64 = null;
+        private bool _useBubble = false;
+
+        private QrCodeBuilder(string url)
+        {
+            _url = url;
+        }
+
+        public static QrCodeBuilder Create(string url) => new QrCodeBuilder(url);
+
+        public QrCodeBuilder WithPixelSize(int size)
+        {
+            _pixelSize = size;
+            return this;
+        }
+
+        private bool _transparentBackground = false;
+
+        public QrCodeBuilder WithTransparentBackground(bool transparent)
+        {
+            _transparentBackground = transparent;
+            return this;
+        }
+
+        public QrCodeBuilder WithColors(string fg, string bg)
+        {
+            _foregroundColor = fg;
+            _backgroundColor = bg;
+            return this;
+        }
+
+        public QrCodeBuilder WithLogo(string logoBase64)
+        {
+            _logoBase64 = logoBase64;
+            return this;
+        }
+
+        public QrCodeBuilder WithBubbleStyle(bool useBubble)
+        {
+            _useBubble = useBubble;
+            return this;
+        }
+
+        public Bitmap Build()
+        {
+            Bitmap qrBitmap = _useBubble ? GenerateBubbleQrCode() : GenerateStandardQrCode();
+
+            if (!string.IsNullOrWhiteSpace(_logoBase64))
+            {
+                byte[] logoBytes = Convert.FromBase64String(_logoBase64);
+                using var logoStream = new MemoryStream(logoBytes);
+                using var logo = new Bitmap(logoStream);
+                int logoSize = qrBitmap.Width / 5;
+                using var resizedLogo = new Bitmap(logo, new Size(logoSize, logoSize));
+
+                using Graphics g = Graphics.FromImage(qrBitmap);
+                int x = (qrBitmap.Width - resizedLogo.Width) / 2;
+                int y = (qrBitmap.Height - resizedLogo.Height) / 2;
+                g.DrawImage(resizedLogo, x, y, resizedLogo.Width, resizedLogo.Height);
+            }
+
+            return qrBitmap;
+        }
+
+        private Bitmap GenerateStandardQrCode()
+        {
+            using var generator = new QRCodeGenerator();
+            using var qrData = generator.CreateQrCode(_url, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new PngByteQRCode(qrData);
+            byte[] pngBytes = qrCode.GetGraphic(_pixelSize);
+
+            using var ms = new MemoryStream(pngBytes);
+            using var bitmap = new Bitmap(ms);
+
+            return RecolorBitmap(bitmap, _foregroundColor, _backgroundColor, _transparentBackground);
+        }
+
+        private Bitmap GenerateBubbleQrCode()
+        {
+            using var generator = new QRCodeGenerator();
+            var qrData = generator.CreateQrCode(_url, QRCodeGenerator.ECCLevel.Q);
+            var modules = qrData.ModuleMatrix;
+            int moduleSize = _pixelSize;
+            int size = modules.Count * moduleSize;
+            var bitmap = new Bitmap(size, size);
+            var fgColor = ColorTranslator.FromHtml(_foregroundColor);
+            var bgColor = ColorTranslator.FromHtml(_backgroundColor);
+
+            using var g = Graphics.FromImage(bitmap);
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.Clear(_transparentBackground ? Color.Transparent : bgColor);
+            using var brush = new SolidBrush(fgColor);
+
+            for (int y = 0; y < modules.Count; y++)
+            {
+                for (int x = 0; x < modules.Count; x++)
+                {
+                    if (modules[y][x])
+                    {
+                        int px = x * moduleSize;
+                        int py = y * moduleSize;
+                        g.FillEllipse(brush, px, py, moduleSize, moduleSize);
+                    }
+                }
+            }
+
+            return bitmap;
+        }
+
+        private Bitmap RecolorBitmap(Bitmap original, string fgHex, string bgHex, bool transparent)
+        {
+            var fg = ColorTranslator.FromHtml(fgHex);
+            var bg = ColorTranslator.FromHtml(bgHex);
+
+            var recolored = new Bitmap(original.Width, original.Height, PixelFormat.Format32bppArgb);
+            using Graphics g = Graphics.FromImage(recolored);
+    
+            if (!transparent)
+                g.Clear(bg);
+            else
+                g.Clear(Color.Transparent);
+
+            for (int y = 0; y < original.Height; y++)
+            {
+                for (int x = 0; x < original.Width; x++)
+                {
+                    var pixel = original.GetPixel(x, y);
+                    if (pixel.R < 128)
+                        recolored.SetPixel(x, y, fg);
+                }
+            }
+
+            return recolored;
+        }
+
+    }
+}
